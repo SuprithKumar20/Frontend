bf2a03fd2a9c6739d83b89b0039201478748bf48
deepak0deepz <deepakcs2k4@gmail.com>
2026-04-14 12:04:47 +0530

    updated

--- CRUD/01-CREATE.js
+++ CRUD/01-CREATE.js
@@ -7,8 +7,8 @@ let person = {
     city : "Bengaluru"
 };
 console.log("-------------BEFORE INSERT-----------------");
-console.log(person); // { name: 'Deepak C S', age: 22, city: 'Bengaluru' }
+console.log(person);                // { name: 'Deepak C S', age: 22, city: 'Bengaluru' }
 console.log("-------------AFTER INSERT-----------------");
 person.dept="AIML"
 person["college"] = "CEC";
-console.log(person); // { name: 'Deepak C S', age: 22, city: 'Bengaluru', dept: 'AIML', college: 'CEC' }
+console.log(person);                // { name: 'Deepak C S', age: 22, city: 'Bengaluru', dept: 'AIML', college: 'CEC' }
