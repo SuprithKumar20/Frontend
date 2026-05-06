d01dcaa88c8b1f8b2aaa6ef21d9c12b8dfc77c46
SuprithKumar20 <suprithkumar2021@gmail.com>
2026-05-06 10:10:04 +0530

    update

--- js_Programming/asynchronous.js
+++ js_Programming/asynchronous.js
@@ -19,3 +19,82 @@
 // }
 // demo();
 // console.log("end")
+
+
+                                        //! PROMISE IN JS
+
+// these are the way to handle the asynchronous operations.
+// a promise is a JS object that links producing code and consuming the code that represent a value 
+// may be available now or in the ufture or never
+
+// // The promise obejct can be in any 3 states:
+// 1.pending
+// 2.fulfilled
+// 3.rejected
+
+
+//                                         promises
+//                                             |
+//                                             |
+//     ---------------------------------------------------------------------------
+//     |                                        |                                |   
+// fulfilled                               pending                            rejected
+//     |                                                                         |
+// resolved()                                                                 reject()
+//     |                                                                         |
+//   then()                                                                    catch()
+
+
+//!creating a promise
+
+// let promissDemo=new Promise((resolve,reject)=>{
+//     if(false){
+//         resolve("promise resolved succeffully")
+//     }else{
+//         reject("promise rejected")
+//     }
+// });
+
+//!Handling a promise
+// proimiseDemo.then((result)=>{
+//     console.log(result);
+// }).catch((error)=>{
+//     console.log(error);
+// });
+
+
+                                                //! FETCH()
+// fetch is a method which works internally as a promise .fetch method is used to make a netwrok request or to call a API in JS.
+// fetch will return a promise in JS.
+
+// fetch('https://fakestoreapi.com/products/1')//make api call
+// .then((res)=>res.json())//return promise fullfill
+// .then((jsondata)=>{
+//     console.log(jsondata)
+// })//fulfill
+// .catch((error)=>{
+//     console.log(error+"promise failed")
+// })//rejected
+
+                                    // ! async and await
+// // async:
+// declare a function or method as asynchronous and can pause its execution to wait for completion of other process.
+
+// // await:
+// make a suspension pont where exceution may wait for the result of async function or methods.
+
+
+async function fun1(){
+    //fetch the data from the URL
+    let response =await fetch("'https://fakestoreapi.com/products/1");
+    //convert the response to json
+    let jsonData=await response.json();
+    //Log the Json data
+    console.log(jsonData);
+    //You can also assign jsonData to a varaiable if you want to use it later
+    let result =jsonData;
+    //log the result
+    console.log(result);
+    //call the asyn fucntion
+}
+fun1();
